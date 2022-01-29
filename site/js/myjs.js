efdc25bcb6cddbdccd5321883980728299cb81bc
queen of mountains <qu33n0fm0unt41ns@gmail.com>
2022-01-28 19:30:54 -0500

    Pages

---
+++ site/js/myjs.js
@@ -0,0 +1,9 @@
+ <script type="text/javascript">  
+    function playMusic(){
+    var music = new Audio('musicfile.mp3');
+    music.play();
+    }
+    function holMundo(params) {
+        alert('1')
+    }
+</script>  
